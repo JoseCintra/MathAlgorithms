082c754d6395ed1a6e50f2a38b78872e5401b5f8
José Augusto Cintra <jose.augusto.cintra@gmail.com>
2020-12-20 15:42:39 -0300

    Add files via upload

--- Arithmetic/Factorial/fatorialTest.f95
+++ Arithmetic/Factorial/fatorialTest.f95
@@ -1,16 +1,18 @@
-! File: factorialTest.f95 
-! Purpose: Function that calculates the factorial of a given number and a test program
+! File:          factorialTest.f95
+! Purpose:       Function that calculates the factorial of a given number and a test program
+! Obs:           The factorial is calculated in two ways: iterative and recursive
 ! documentation: https://github.com/JoseCintra/MathAlgorithms
-! Language: Fortran 95
-! Author: José Cintra
-! Date: 2020, December
-! License: Unlicense, described in http://unlicense.org
+! Language:      Fortran 95
+! Author:        José Cintra
+! Date:          2020, December
+! License:       Unlicense, described in http://unlicense.org
 
 MODULE math
+! Mathmathical functions
 
 CONTAINS
 
-   ! Function to calculate the factorial
+   ! Function to calculate the factorial in a iterative way
    INTEGER FUNCTION factorial(num)
 
       ! Variables
@@ -26,6 +28,22 @@ CONTAINS
       RETURN
 
    END FUNCTION factorial
+
+  ! Function to calculate the factorial in a recursive way
+  RECURSIVE FUNCTION recFactorial (num) RESULT (res)
+
+    ! Variables
+    INTEGER, INTENT(IN) :: num
+    INTEGER :: res
+
+    !Calculation
+    IF (num <= 1) THEN
+       res = 1
+    ELSE
+       res = recFactorial(num-1) * num
+    END IF
+
+  END FUNCTION recFactorial
 
 END MODULE
 
@@ -54,6 +72,9 @@ PROGRAM factorialTest
       ! Output
       fact = factorial(num)
       WRITE (*, '(a,I0,a,I0)') "The factorial of the number ", num, " is ", fact
+      fact = recFactorial(num)
+      WRITE (*, '(a,I0,a,I0)') "The recursive factorial of the number ", num, " is ", fact
+
    END DO
    PRINT *,""
    WRITE (*, '(a)') "END of execution"
