70a03bb0534aaaa26d9febbd7cb48c3ad850b667
José Augusto Cintra <jose.augusto.cintra@gmail.com>
2022-10-28 07:08:15 -0300

    Algorithms

---
+++ Algorithms/num_divisors.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+   "fmt"
+   "math"
+)
+
+/*
+
+   Function: Determines whether the given number (num) is prime or not, using the trial division (6k + 1) method
+   Input:
+      num - Number to test (integer)
+   Output:
+      ret: Return value (true or false)
+      err: Error condition - NOT USED. Only for compatibility reasons
+   Theoretical foundation: https://en.wikipedia.org/wiki/Primality_test
+   Language: GO
+   Date: Spring 2022
+   Author: José Cintra - https://github.com/JoseCintra/MathAlgorithms
+
+*/
+
+func IsPrimeByDiv(num uint64) (ret bool, err bool) {
+
+   var i uint64   // Loop control variable
+   var lim uint64 // Loop limit
+   err = false
+   ret = true
+
+   if (num == 2) || (num == 3) {
+      goto finish
+   }
+
+   if (num <= 1) || (num%2 == 0) || (num%3 == 0) {
+      ret = false
+      goto finish
+   }
+
+   lim = uint64(math.Sqrt(float64(num)))
+   for i = 5; i <= lim; i += 6 {
+      if (num%i == 0) || (num%(i+2) == 0) {
+         ret = false
+         break
+      }
+   }
+
+finish:
+   return
+
+}
+
+/*
+
+   Function: Determines whether the given number (num) is even or not, using bitwise operation
+   Input:
+      num - Number to test (integer)
+   Output:
+      ret: Return value (true or false)
+   Theoretical foundation: https://www.codeproject.com/Articles/1005626/Ways-to-Check-whether-the-Given-Integer-is-Even-or
+   Language: GO
+   Date: Spring 2022
+   Author: José Cintra - https://github.com/JoseCintra/MathAlgorithms
+
+*/
+func isEven(num uint64) (ret bool) {
+   ret = ((num & 1) == 0)
+   return
+}
+
+/*
+
+   Function: Given a natural number, find the next prime number greater than it
+   Input:
+      num - a natural number
+   Output:
+      ret: Return value - the next prime
+      err: Error condition - Overflow
+   Date: Spring 2022
+   Language: GO
+   Author: José Cintra - https://github.com/JoseCintra/MathAlgorithms
+
+*/
+func nextPrime(num uint64) (ret uint64, err bool) {
+
+   ret = 0
+   var i uint64
+   if num <= 1 {
+      ret = 2
+      goto finish
+   }
+   if isEven(num) {
+      num++
+   } else {
+      num += 2
+   }
+   for i = num; i < math.MaxUint64; i += 2 {
+      res, _ := IsPrimeByDiv(i)
+      if res {
+         ret = i
+         break
+      }
+
+   }
+   if ret == 0 {
+      err = true // Overflow
+   }
+finish:
+   return
+}
+
+/*
+
+   Function: Given a natural number N, calculate all its prime factors
+   Input:
+      num - Number to test (integer)
+   Output:
+      ret: Return value - a map with the prime factors and your exponents
+      err: Error condition - NOT USED. Only for compatibility reasons
+   Theoretical foundation: https://brilliant.org/wiki/prime-factorization/
+   Language: GO
+   Date: Spring 2022
+   Author: José Cintra - https://github.com/JoseCintra/MathAlgorithms
+
+*/
+func PrimeFactors(num uint64) (ret map[uint64]uint64, err bool) {
+   var div uint64 = 0 // Prime factor
+   var i uint64 = 1   // Loop Control
+   var exp uint64 = 0 // Exponent of fator or quantity of each factor
+   ret = make(map[uint64]uint64)
+   err = false
+
+   isPrime, _ := IsPrimeByDiv(num)
+   if isPrime {
+      ret[num] = 1
+      goto finish
+   }
+   if num <= 1 {
+      err = true
+      goto finish
+   }
+
+   for num > 1 {
+      div, _ = nextPrime(i)
+      exp = 0
+      for num%div == 0 {
+         exp++
+         num = num / div
+      }
+
+      if exp != 0 {
+         ret[div] = exp
+      }
+      i++
+   }
+finish:
+   return
+}
+
+/*
+
+   Function: Calculates the number of divisors of a given number by prime factorization
+   Input:
+      num - Number to test (integer)
+   Output:
+      ret: Return value representing the number o divisors
+      err: Error condition - NOT USED. Only for compatibility reasons
+   Theoretical foundation: https://en.wikipedia.org/wiki/Divisor
+   Language: GO
+   Date: Spring 2022
+   Author: José Cintra - https://github.com/JoseCintra/MathAlgorithms
+
+*/
+func getNumDivs(num uint64) (ret uint64, err bool) {
+   pf, _ := PrimeFactors(num) // Prime Factors of num
+   ret = 1
+   for _, exp := range pf {
+      ret *= (exp + 1)
+   }
+   return
+}
+
+func main() {
+   fmt.Println("Table of Divisors - Numbers of 1 to 100")
+   fmt.Println("")
+   var i uint64
+   fmt.Printf("\n%-5s\t%-5s", "n", "d(n)")
+   fmt.Printf("\n%-5s\t%-5s \n", "-----", "-----")
+   for i = 1; i <= 100; i++ {
+      numDiv, _ := getNumDivs(i)
+      fmt.Printf("%5d\t%5d \n", i, numDiv)
+   }
+}
